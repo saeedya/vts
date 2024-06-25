453e7fd42a43ee8005f1dd912b5cd671d9cb4025
Saeed <yasrebi.saeed@gmail.com>
2024-06-25 23:01:07 +0100

    Adding user_sdervice model.

--- user_service/app.py
+++ user_service/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from config import Config
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
+    api = Api(app)
     return app
